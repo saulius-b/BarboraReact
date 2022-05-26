e2e580439421aaa9fe45e7332989b712f1707783
Andrius Lesevicius <66474781+andrius2500@users.noreply.github.com>
2022-05-26 23:57:30 +0300

    Update PageSelect.js

--- src/App/Main/ItemPlaceholder/PageSelect.js
+++ src/App/Main/ItemPlaceholder/PageSelect.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-function PageSelect() {
+function PageSelect() { --- tas pats komentaras kaip ir Filter.js
   return (
     <section id='PageSelect'>PageSelect</section>
   )
