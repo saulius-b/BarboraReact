7ed5d1fe499ae55d9f7119b26cd02642470a4f85
Andrius Lesevicius <66474781+andrius2500@users.noreply.github.com>
2022-05-26 23:57:09 +0300

    Update Filter.js

--- src/App/Main/ItemPlaceholder/Filter.js
+++ src/App/Main/ItemPlaceholder/Filter.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-function Filter() {
+function Filter() { ---is pavadinimo suprantu, kas cia turi buti, bet siuo metu nemanau, kad reik jo, nes komponentai tusti ir nemanau, kad tai parodo, ka moki. Taip kad trink lauk
   return (
     <section id='Filter'>Filter</section>
   )
