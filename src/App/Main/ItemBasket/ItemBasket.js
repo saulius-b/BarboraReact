5d69af87b6c8cf5652e3d792da8d3c1e2d39cb31
Andrius Lesevicius <66474781+andrius2500@users.noreply.github.com>
2022-05-26 23:52:52 +0300

    Update ItemBasket.js

--- src/App/Main/ItemBasket/ItemBasket.js
+++ src/App/Main/ItemBasket/ItemBasket.js
@@ -3,19 +3,16 @@ import BasketItem from '../ItemBasket/BasketItem'
 import BasketBottom from '../ItemBasket/BasketBottom'
 
 
-function ItemBasket({productList, addItem, removeItem, totalPrice}) {
+function ItemBasket({productList, addItem, removeItem, totalPrice}) { --- arrow function
 
   const basketItem = productList.map((item, index) => (
-    item.addToCart  === true && item.count > 0 ? 
-    <BasketItem key={item.id} index={index} image={item.image} itemName={item.itemName} itemPrice={item.itemPrice} count={item.count} addItem={addItem} removeItem={removeItem} /> : null
+    item.addToCart && item.count > 0 && 
+    <BasketItem key={item.id} index={index} image={item.image} itemName={item.itemName} itemPrice={item.itemPrice} count={item.count} addItem={addItem} removeItem={removeItem} />
   ))
-
-  
 
   return (
     <aside id='ItemBasket'>
       {basketItem}    
-     
       <BasketBottom productList={productList} totalPrice={totalPrice} />
     </aside>
   )
