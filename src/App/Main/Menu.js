72f694c3de52dcdc88189388738ae464829a6d19
Andrius Lesevicius <66474781+andrius2500@users.noreply.github.com>
2022-05-26 23:50:15 +0300

    Update Menu.js

--- src/App/Main/Menu.js
+++ src/App/Main/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-function Menu() {
+function Menu() { --- manau, kad gali trinti lauk ir ideti ji i koki kita komponenta
   return (
     <nav id='Menu'>Menu</nav>
   )
